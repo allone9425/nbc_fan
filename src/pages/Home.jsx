22adc23be1a869e4b63d12dfcf10cdcc106e4104
allone9425 <allone9425@gmail.com>
2023-11-10 15:44:09 +0900

    내일배움캠프 팬레터 컬렉션

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,39 @@
-import React, { Component } from "react";
+import styled from "styled-components";
 
-export class Home extends Component {
-  render() {
-    return <div>여기는 Home</div>;
-  }
+function Home() {
+  const Header = styled.header`
+    background-color: #ffc8dd;
+    padding: 100px;
+  `;
+
+  const Title = styled.h2`
+    font-size: 30px;
+    text-align: center;
+    margin: 30px 0;
+    font-weight: bold;
+  `;
+
+  const Memberbox = styled.ul`
+    display: flex;
+    justify-content: center;
+  `;
+  const Name = styled.li`
+    background-color: #cdb4db;
+    padding: 10px 5px;
+    margin: 0 10px;
+  `;
+  return (
+    <div>
+      <Header>
+        <Title>아티스트 컬렉션</Title>
+        <Memberbox>
+          <Name>철수</Name>
+          <Name>영희</Name>
+          <Name>길고양이</Name>
+        </Memberbox>
+      </Header>
+    </div>
+  );
 }
+
 export default Home;
